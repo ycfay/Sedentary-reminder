692f1463254a114a33ee84796216a7e0ca782abb
richie <zhangpeng@szjzsoft.com>
2026-04-16 12:25:52 +0800

    202604161225 V1

--- Reminder/AboutBox.Designer.cs
+++ Reminder/AboutBox.Designer.cs
@@ -52,7 +52,7 @@
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 14);
             this.label2.TabIndex = 1;
-            this.label2.Text = "v1.3.0";
+            this.label2.Text = "v1.5.0";
             // 
             // label3
             // 
