d8060f6f25f014d5e6451ffc919acbac9d0a0add
richie <zhangpeng@szjzsoft.com>
2026-04-16 13:38:29 +0800

    202604161338 V11

--- Reminder/MainFrm.Designer.cs
+++ Reminder/MainFrm.Designer.cs
@@ -58,7 +58,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 40);
+            this.label1.Location = new System.Drawing.Point(27, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 12);
             this.label1.TabIndex = 0;
@@ -68,7 +68,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(12, 11);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(67, 14);
             this.label2.TabIndex = 3;
@@ -77,7 +77,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(27, 67);
+            this.label3.Location = new System.Drawing.Point(27, 69);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(65, 12);
             this.label3.TabIndex = 5;
@@ -88,7 +88,7 @@
             this.btn_start.BackColor = System.Drawing.Color.Transparent;
             this.btn_start.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
             this.btn_start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_start.Location = new System.Drawing.Point(58, 229);
+            this.btn_start.Location = new System.Drawing.Point(58, 231);
             this.btn_start.Name = "btn_start";
             this.btn_start.Size = new System.Drawing.Size(75, 23);
             this.btn_start.TabIndex = 11;
@@ -99,7 +99,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(147, 41);
+            this.label5.Location = new System.Drawing.Point(147, 43);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 12);
             this.label5.TabIndex = 14;
@@ -108,7 +108,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(147, 68);
+            this.label6.Location = new System.Drawing.Point(147, 70);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(53, 12);
             this.label6.TabIndex = 15;
@@ -117,7 +117,7 @@
             // numWrkTime
             // 
             this.numWrkTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.numWrkTime.Location = new System.Drawing.Point(97, 37);
+            this.numWrkTime.Location = new System.Drawing.Point(97, 39);
             this.numWrkTime.Maximum = new decimal(new int[] {
             120,
             0,
@@ -136,7 +136,7 @@
             // 
             this.numRstTime.BackColor = System.Drawing.Color.White;
             this.numRstTime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.numRstTime.Location = new System.Drawing.Point(96, 64);
+            this.numRstTime.Location = new System.Drawing.Point(96, 66);
             this.numRstTime.Maximum = new decimal(new int[] {
             30,
             0,
@@ -193,7 +193,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label4.Location = new System.Drawing.Point(12, 94);
+            this.label4.Location = new System.Drawing.Point(12, 96);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(52, 14);
             this.label4.TabIndex = 18;
@@ -204,7 +204,7 @@
             this.ckBoxInput.AutoSize = true;
             this.ckBoxInput.Checked = true;
             this.ckBoxInput.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.ckBoxInput.Location = new System.Drawing.Point(29, 123);
+            this.ckBoxInput.Location = new System.Drawing.Point(29, 125);
             this.ckBoxInput.Name = "ckBoxInput";
             this.ckBoxInput.Size = new System.Drawing.Size(144, 16);
             this.ckBoxInput.TabIndex = 19;
@@ -213,7 +213,7 @@
             // 
             // ckBoxAutoStart
             // 
-            this.ckBoxAutoStart.Location = new System.Drawing.Point(29, 148);
+            this.ckBoxAutoStart.Location = new System.Drawing.Point(29, 150);
             this.ckBoxAutoStart.Name = "ckBoxAutoStart";
             this.ckBoxAutoStart.Size = new System.Drawing.Size(144, 16);
             this.ckBoxAutoStart.TabIndex = 20;
@@ -223,7 +223,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(27, 175);
+            this.label7.Location = new System.Drawing.Point(27, 177);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(77, 12);
             this.label7.TabIndex = 21;
@@ -231,7 +231,7 @@
             // 
             // trackBar
             // 
-            this.trackBar.Location = new System.Drawing.Point(99, 171);
+            this.trackBar.Location = new System.Drawing.Point(99, 173);
             this.trackBar.Minimum = 1;
             this.trackBar.Name = "trackBar";
             this.trackBar.Size = new System.Drawing.Size(104, 45);
