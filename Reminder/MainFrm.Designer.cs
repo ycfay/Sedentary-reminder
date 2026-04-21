244d3938dd4482514b4414fea9d8147da54675af
richie <zhangpeng@szjzsoft.com>
2026-04-21 09:35:24 +0800

    20260421 935 fix ico bug

--- Reminder/MainFrm.Designer.cs
+++ Reminder/MainFrm.Designer.cs
@@ -270,6 +270,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainFrm";
